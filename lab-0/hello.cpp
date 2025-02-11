42e99b0d5cc4f654e71118104af7603fd72462d8
lazydukk <banul07@proton.me>
2025-02-11 21:13:56 +0530

    day 02

--- lab-0/hello.cpp
+++ lab-0/hello.cpp
@@ -1,5 +1,8 @@
 #include <iostream> // this is the most basic header file
+#include <vector>
 
+typedef std::string text_t;
+typedef int number_t;
 // main function
 int main()
 {
@@ -11,8 +14,23 @@ int main()
 
     double gpa = 3.9;
     double temp = 27.6;
-    std::string name = "John";
+    std::string name = "Bob";
     std::cout << "Hello " << name << '\n';
 
+    text_t say_it = "Hello from typedef";
+    number_t numb_it = 69;
+    std::cout << say_it << '\n';
+    std::cout << numb_it << '\n';
+
+    int students = 18;
+    // students = students + 1;
+    students += 1;
+    students++; // only adds one to the var
+    std::cout << "Number of T students " << students << '\n';
+    int remainder = students % 3; // remainder operator
+    std::cout << "Students left after dividing into groups of 3 = " << remainder << '\n';
+
+    int mod_op = 2 % 2;
+    std::cout << "Remainder = " << mod_op << '\n';
     return 0;
 }
