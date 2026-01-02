f1fbb50eec7c20f94bd0d181e19563369b843751
lazydukk <bytex07@proton.me>
2026-01-02 16:28:03 +0530

    Create loops.cpp

---
+++ lab-1/loops.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //for loop
+    // counts untill i = 4
+    for (int i = 0; i < 5; i++) {
+        cout << "for: " << i << "\n";
+    }
+
+    // while loop
+    int a = 0;
+    while (a < 5) {
+        cout << "while: " << a << "\n";
+        a++;
+    }
+
+    // do-while loop
+    int b = 0;
+    do {
+        cout << "do-while: " << b << "\n";
+        b++;
+    }while (b < 5);
+    
+    return 0;
+}
