fcc97a8171886ab493e09d89eb788259d685f33b
lazydukk <bytex07@proton.me>
2026-01-06 12:42:17 +0530

    Update pointers-references.cpp

--- lab-1/pointers-references.cpp
+++ lab-1/pointers-references.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int add(int a, int b) {
-    return a * b;
-}
+int add(int a, int b) { return a * b; }
 
 int main() {
-   int num = 10;
-   int *ptr = &num; // pointer ptr now points to the memory address of the num
-   int value = *ptr;
-   cout << value << endl;
+  int num = 10;
+  int *ptr = &num; // pointer ptr now points to the memory address of the num
+  int value = *ptr;
+  cout << value << endl;
 
-   int (*funcpntr) (int, int) = add;
-   funcpntr(4, 5);
-   cout << add << endl;
+  int (*funcpntr)(int, int) = add;
+  funcpntr(4, 5);
+  cout << add << endl;
 
-    return 0;
+  return 0;
 }
