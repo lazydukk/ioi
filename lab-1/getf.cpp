5c2b40ca804405a830981c1f982c6d5ef7334aa6
lazydukk <bytex07@proton.me>
2026-01-06 12:42:41 +0530

    Update getf.cpp

--- lab-1/getf.cpp
+++ lab-1/getf.cpp
@@ -1,12 +1,11 @@
-#include <string>
-#include <iostream>
 #include <filesystem>
+#include <iostream>
+#include <string>
 using namespace std;
 namespace fs = std::filesystem;
 
-int main()
-{
-    string path = "D:\\obsidian\\log0\\Vocab\\en-US";
-    for (const auto & entry : fs::directory_iterator(path))
-        cout << entry.path() << endl;
+int main() {
+  string path = "D:\\obsidian\\log0\\Vocab\\en-US";
+  for (const auto &entry : fs::directory_iterator(path))
+    cout << entry.path() << endl;
 }
