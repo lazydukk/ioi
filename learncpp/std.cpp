f3712395b68a97350daabebe62afeb83c107f7c4
lazydukk <bytex07@proton.me>
2026-01-30 12:37:39 +0530

    Update std.cpp

--- learncpp/std.cpp
+++ learncpp/std.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 int main() {
   int x{5};
-  cout << "x is equal to " << x;
+  cout << "x is equal to " << x << '\n';
+  cout << "Enter a number: ";
+  int y{};
+  cin >> y;
+  cout << "you entered: " << y << '\n';
   return 0;
 }
