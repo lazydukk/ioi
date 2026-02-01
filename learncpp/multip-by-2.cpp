4e754ead1df2b543c18f4b4eedc1db50f9aa1eb1
lazydukk <bytex07@proton.me>
2026-02-01 19:08:19 +0530

    Update multip-by-2.cpp

--- learncpp/multip-by-2.cpp
+++ learncpp/multip-by-2.cpp
@@ -6,6 +6,7 @@ int main()
     cout << "Enter an integer: ";
     int num{};
     cin >> num;
-    cout << "Double that number is: " << num * 2 << '\n'; //use an expression to multiply num * 2 at the point where we are going to print it
+    cout << "Double " << num << " is " << num * 2 << '\n';      //use an expression to multiply num * 2 at the point where we are going to print it
+    cout << "Tripple " << num << " is "  << num * 3 << '\n';
     return 0;
 }
