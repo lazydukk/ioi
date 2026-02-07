7b060dd001546acaa4053494456fe145a1a7f526
lazydukk <bytex07@proton.me>
2026-02-07 19:29:20 +0530

    add CPH

---
+++ CPH/inout.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // makes input and output more efficient
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  string s{};
+  getline(cin, s);
+  cout << s << '\n';
+  return 0;
+}
