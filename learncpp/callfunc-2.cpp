2b2c9d3fac025384c5817a3380905ba415b6e49e
lazydukk <bytex07@proton.me>
2026-02-07 18:19:10 +0530

    update

--- learncpp/callfunc-2.cpp
+++ learncpp/callfunc-2.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void doB()
-{
-    cout << "In doB() \n";
-}
+void doB() { cout << "In doB() \n"; }
 
 void doA() {
   cout << "starting doA() \n";
@@ -13,10 +10,9 @@ void doA() {
 }
 
 // definition of main function
-int main()
-{
-    cout << "Starting main() \n";
-    doA();
-    cout << "Ending main() \n";
-    return 0;
+int main() {
+  cout << "Starting main() \n";
+  doA();
+  cout << "Ending main() \n";
+  return 0;
 }
