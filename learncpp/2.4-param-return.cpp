9bf9bf2cfd0d253ea8d9cf6f58317c36df056037
lazydukk <bytex07@proton.me>
2026-02-08 22:50:53 +0530

    Update 2.4-param-return.cpp

--- learncpp/2.4-param-return.cpp
+++ learncpp/2.4-param-return.cpp
@@ -5,7 +5,16 @@ using namespace std;
 // The values of x and y are determined by the function that calls add()
 int add(int x, int y) { return x + y; }
 
+int multiply(int a, int b) { return a * b; }
+
 int main() {
   cout << add(4, 5) << '\n'; // arg 4 and 5 are passed to the add() fucntion
+  cout << add(1 + 2, 3 * 4) << '\n'; // x = 1 +2 = 3; y = 3 * 4 = 12;
+
+  int z{5};
+  cout << add(z, z) << '\n'; // evaluates to ( 5 + 5) = 10;
+
+  cout << add(1, multiply(2, 3)) << '\n'; // evals to 1 + 6
+  cout << add(1, add(2, 3)) << '\n';      // evals to 1 + 5
   return 0;
 }
