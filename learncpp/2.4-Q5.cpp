c34ed4ed7a5c75ea22a742ee64700b679236c5a0
lazydukk <bytex07@proton.me>
2026-02-08 23:21:31 +0530

    Create 2.4-Q5.cpp

---
+++ learncpp/2.4-Q5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// Write a complete program that reads an integer from the user,
+// doubles it using the doubleNumber() function you wrote in the previous quiz
+// question, and then prints the doubled value out to the console.
+
+int doubleNumber(int x) { return 2 * x; }
+
+int main() {
+  cout << "Enter a number: ";
+  int x{};
+  cin >> x;
+  cout << doubleNumber(y) << '\n';
+  return 0;
+}
