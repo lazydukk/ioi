f81170ca88b9cd7e10d9fb03f3b3f348aa26479f
lazydukk <bytex07@proton.me>
2026-02-08 22:22:35 +0530

    Update 2.4-func-para-arg.cpp

--- learncpp/2.4-func-para-arg.cpp
+++ learncpp/2.4-func-para-arg.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
 
+// this function has two integer parameters called x and y;
+// then the values x and y are passed in by the caller
 void printValue(int x, int y) {
   cout << x << '\n';
   cout << y << '\n';
 }
 
 int main() {
-  printValue(6, 7);
+  printValue(6, 7); // this function call has 2 arguments; 6 and 7
   return 0;
 }
