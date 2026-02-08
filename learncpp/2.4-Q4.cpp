14f8cbcd57897c6ea72da81b13bb561878406205
lazydukk <bytex07@proton.me>
2026-02-08 23:17:01 +0530

    Create 2.4-Q4.cpp

---
+++ learncpp/2.4-Q4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+// Write a function called doubleNumber() that takes one integer parameter.
+// The function should return double the value of the parameter.
+
+int doubleNumber(int x) { return 2 * x; }
+
+int main() { cout << doubleNumber(5) << '\n'; }
