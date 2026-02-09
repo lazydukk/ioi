6d5d59a9923f6a1108c5a433da0415698e931c65
lazydukk <bytex07@proton.me>
2026-02-09 21:29:08 +0530

    Update 2.5-local-scope.cpp

--- learncpp/2.5-local-scope.cpp
+++ learncpp/2.5-local-scope.cpp
@@ -10,6 +10,6 @@ int main() {
   int a{5}; // a is created, initialized and enters the scope here
   int b{6}; // b is created, inititalized and enters teh scope here
 
-  cout << add(a, b) << '\n';
+  cout << add(a, b) << '\n'; // calls add(5,6), where x = 5 and y = 6
   return 0;
-}
+} // b and a go out of scope and are destroyed here
