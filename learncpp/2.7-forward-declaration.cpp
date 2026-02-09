8e70140da00a3198250a09235d5aa7d48432ce68
lazydukk <bytex07@proton.me>
2026-02-09 22:21:53 +0530

    Update 2.7-forward-declaration.cpp

---
+++ learncpp/2.7-forward-declaration.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// fucntion declaration
+int add(int x, int y); // forward declaration of add() using a func delcaration
+
+int main() {
+  std::cout << "The sum of 3 and 4 is: " << add(3, 4)
+            << '\n'; // this works because we forward declared add() above
+  return 0;
+}
+
+int add(int x, int y) // even though the body of add() isn't defined until here
+{
+  return x + y;
+}
