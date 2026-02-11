2d1c4ca24b35aa03a6b4d780eb0c792882f3e375
lazydukk <bytex07@proton.me>
2026-02-12 00:01:01 +0530

    Create 2.10-if-zero.cpp

---
+++ learncpp/2.10-if-zero.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Joe\n";
+
+#if 0  // Don't compile anything starting here
+    std::cout << "Bob\n";
+    std::cout << "Steve\n";
+#endif // until this point
+
+  return 0;
+}
