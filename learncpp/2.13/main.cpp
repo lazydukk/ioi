f05075c64762b106c1db2e146500cb18a240b589
lazydukk <bytex07@proton.me>
2026-02-18 18:15:51 +0530

    Create main.cpp

---
+++ learncpp/2.13/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+// implementation of the getUserInput function
+int getUserInput() {
+  std::cout << "Enter an integer: ";
+  int input{};
+  std::cin >> input;
+
+  return input;
+}
+
+
+
+int main() {
+  // get first number from the user
+  // // getUserInput();
+
+  // get mathematical operation from the user
+  // getMathOperation();
+
+  // get second number from the user
+  // getUserInput();
+
+  // calculate the result
+  // calculateResult();
+
+  // print result
+  // printResult();
+  return 0;
+}
