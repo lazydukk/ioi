b53e6597d558fcc2d7eebe43cec50f83930506f2
lazydukk <bytex07@proton.me>
2026-02-20 12:28:46 +0530

    update .gitignore && add log-files from chapter 3

---
+++ learncpp/3.5/log-files/main.cpp
@@ -0,0 +1,24 @@
+#include "plog/Initializers/RollingFileInitializer.h"
+#include <iostream>
+#include <plog/Log.h> // step 1: include the loggger headers
+
+int getUserInput() {
+  PLOGD << "getUserInput() called"; // PLOGD is defined by the plog library
+
+  std::cout << "Enter a number: ";
+  int x{};
+  std::cin >> x;
+  return x;
+}
+
+int main() {
+  plog::init( plog::debug, "Logfile.txt" ); // Step 2: initialize the logger
+
+  PLOGD << "main() called"; // Step 3: Output to the log as if you were writing
+                            // to the console
+
+  int x{ getUserInput() };
+  std::cout << "You entered: " << x << '\n';
+
+  return 0;
+}
