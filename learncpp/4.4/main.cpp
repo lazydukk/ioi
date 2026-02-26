67d918629afa67ba479e27a47243e36903ac2429
lazydukk <bytex07@proton.me>
2026-02-26 22:28:32 +0530

    Update main.cpp

--- learncpp/4.4/main.cpp
+++ learncpp/4.4/main.cpp
@@ -1,15 +1,15 @@
+short s; // prefer "short" instead of "short int"
+int i;
+long l;       // prefer "long" instead of "long int"
+long long ll; // prefer "long long" instead of "long long int"
 
+// integer types can also take an optional "signed" keyword
+signed short ss;
+signed int si;
+signed long sl;
+signed long long sll;
+// redundant as integers are signed by default
 
 int main() {
-  short s; // prefer "short" instead of "short int"
-  int i;
-  long l;       // prefer "long" instead of "long int"
-  long long ll; // prefer "long long" instead of "long long int"
-
-  // integer types can also take an optional "signed" keyword
-  signed short ss;
-  signed int si;
-  signed long sl;
-  signed long long sll;
-  // redundant as integers are signed by default
+  return 0;
 }
