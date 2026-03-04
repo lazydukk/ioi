d7e9a6ab3ef628d8b9007703e6af79abb5307e09
lazydukk <bytex07@proton.me>
2026-03-04 16:11:41 +0530

    Update floating.cpp

--- learncpp/4.8/floating.cpp
+++ learncpp/4.8/floating.cpp
@@ -1,3 +1,4 @@
+#include <iomanip> // for output manipulator std::setprecision()
 #include <iostream>
 
 int main() {
@@ -17,5 +18,22 @@ int main() {
   std::cout << 5.0 << '\n';
   std::cout << 6.7f << '\n';
   std::cout << 9876543.21 << '\n'; // will be printed in scientific notation
+
+  std::cout << 9.87654321f << '\n';
+  std::cout << 987.654321f << '\n';
+  std::cout << 987654.321f << '\n';
+  std::cout << 9876543.21f << '\n';
+  std::cout << 0.0000987654321f << '\n';
+
+  std::cout << std::setprecision( 17 ); // show 17 digits of precision
+  std::cout << 3.33333333333333333333333333333333333333f
+            << '\n'; // f suffix means float
+  std::cout << 3.33333333333333333333333333333333333333
+            << '\n'; // no suffix means double
+
+  float f{ 123456789.0f }; // f has 10 significant digits
+  std::cout << std::setprecision( 9 );
+  std::cout << f << '\n';
+
   return 0;
 }
