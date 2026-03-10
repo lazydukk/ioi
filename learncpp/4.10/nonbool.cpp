5c0cd00ac8e18586ca05af7ac198b67544d6a289
lazydukk <bytex07@proton.me>
2026-03-10 22:51:08 +0530

    Update nonbool.cpp

--- learncpp/4.10/nonbool.cpp
+++ learncpp/4.10/nonbool.cpp
@@ -2,9 +2,9 @@
 
 int main() {
   int x{ 4 };
-  if ( x )
-    std::cout << "hi\n";
+  if ( x )               // if x is non-zore/non-empty
+    std::cout << "hi\n"; // if x != 0; then evaluates to TRUE
   else
-    std::cout << "bye\n";
+    std::cout << "bye\n"; // else; FALSE
   return 0;
 }
