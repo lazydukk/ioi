0cb894fa90b4fe6767354286120a37372337d249
lazydukk <bytex07@proton.me>
2026-04-15 23:54:26 +0530

    Update explic_type.cpp

--- learncpp/4.12/explic_type.cpp
+++ learncpp/4.12/explic_type.cpp
@@ -6,6 +6,8 @@ void print( int x ) {
 
 int main() {
   print( static_cast<int>( 5.5 ) ); // explicitly converts double value to an
-                                    // int double value 5.5 => int value 5
+  // int double value 5.5 => int value 5
+  char ch{ 97 };
+  std::cout << ch << " has value " << static_cast<int>( ch ) << '\n';
   return 0;
 }
