3e6cb61572c20503f388790ef7f86eaf2f1ebbb0
lazydukk <bytex07@proton.me>
2026-04-17 17:28:01 +0530

    Update explic_type.cpp

--- learncpp/4.12/explic_type.cpp
+++ learncpp/4.12/explic_type.cpp
@@ -1,3 +1,4 @@
+#include <cstdint>
 #include <iostream>
 
 void print( int x ) {
@@ -9,5 +10,28 @@ int main() {
   // int double value 5.5 => int value 5
   char ch{ 97 };
   std::cout << ch << " has value " << static_cast<int>( ch ) << '\n';
+
+  unsigned int u1{ 5 };
+  // convert value of u1 to a signed int
+  int s1{ static_cast<int>( u1 ) };
+  std::cout << s1 << '\n'; // prints 5
+
+  int s2{ 5 };
+  // convert value of s2 to an unsigned int
+  unsigned int u2{ static_cast<unsigned int>( s2 ) };
+  std::cout << u2 << '\n'; // prints 5
+
+  int s{ -1 };
+  std::cout << static_cast<unsigned int>( s ) << '\n';
+
+  std::cout << "break" << '\n';
+
+  unsigned int u{ 4294967295 }; // largest 32-bit unsigned int
+  std::cout << static_cast<int>( u )
+            << '\n'; // implementation-defined prior to C++20, -1 as of C++20
+  std::int8_t myInt{ 65 };
+  std::cout << myInt << '\n'; // outputs 'A'; treats 'myInt' as a signed char
+  std::cout << static_cast<int>( myInt ) << '\n'; // will always print 65
+
   return 0;
 }
